d379aca6dbf45c4cc4933e27c9bf6cc219d1c311
Syed W. Quadri <swq1999@gmail.com>
2023-05-04 22:34:33 +0100

    Radio Boxes Added to Interface

--- iterative/interface.py
+++ iterative/interface.py
@@ -1,6 +1,6 @@
 import customtkinter as ctk
 from PIL import Image
-from Classifier_in_functions import classify
+from Classifier_in_functions import classifyAlphabets, classifyNumbers
 
 
 class MainWindow:
@@ -36,7 +36,7 @@ class MainWindow:
             master,
             text="Start",
             font=my_font,
-            width=235,
+            width=140,
             height=30,
             command=lambda: start(),
         )
@@ -44,24 +44,52 @@ class MainWindow:
             master,
             text="Exit",
             font=my_font,
-            width=235,
+            width=140,
             height=30,
             command=lambda: close(),
         )
 
+        # setting radio btn values
+        master.var = ctk.IntVar()
+        master.var.set(1)
+
+        master.alphabets = ctk.CTkRadioButton(
+            master,
+            text="Alphabets",
+            font=my_font,
+            variable=master.var,
+            value=1,
+        )
+        master.numbers = ctk.CTkRadioButton(
+            master,
+            text="Numbers",
+            font=my_font,
+            variable=master.var,
+            value=2,
+        )
+
         # add buttons to the grid
         master.image.grid(
-            row=0, column=0, columnspan=2, padx=10, pady=(10,5), sticky="nsew"
+            row=0, column=0, columnspan=4, padx=10, pady=(10,5), sticky="nsew"
+        )
+        master.alphabets.grid(
+            row=1, column=0, columnspan=1, padx=(15,5), pady=(5,10), sticky="nsew"
+        )
+        master.numbers.grid(
+            row=1, column=1, columnspan=1, padx=(5,5), pady=(5,10), sticky="nsew"
         )
         master.start.grid(
-            row=1, column=0, columnspan=1, padx=(10,5), pady=(5,10), sticky="nsew"
+            row=1, column=2, columnspan=1, padx=(5,5), pady=(5,10), sticky="nsew"
         )
         master.exit.grid(
-            row=1, column=1, columnspan=1, padx=(5,10), pady=(5,10), sticky="nsew"
+            row=1, column=3, columnspan=1, padx=(5,10), pady=(5,10), sticky="nsew"
         )
 
         def start():
-            classify()
+            if master.var.get() == 1:
+                classifyAlphabets()
+            elif master.var.get() == 2:
+                classifyNumbers()
 
         def close():
             master.quit()
